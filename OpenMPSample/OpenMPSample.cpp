62e054087d0d6f48b6fa742cecdbdf7de50a9fd4
Ahmed Hamdy <ahmedhamdyau@gmail.com>
2016-04-30 22:19:18 +0200

    Add project files.

---
+++ OpenMPSample/OpenMPSample.cpp
@@ -0,0 +1,24 @@
+// MatrixMultiplicationOpenMp.cpp : main project file.
+
+#include "stdafx.h"
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+	int tenNums[10];
+
+#pragma omp parallel for
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d ", i);	// View the order of execution
+		tenNums[i] = i;
+	}
+	printf("\n");
+	for (int i = 0; i < 10; i++)
+		printf("%d\n", tenNums[i]);
+
+
+	system("pause");	// Halt the program
+	return 0;
+}
---
+++ OpenMPSample/stdafx.cpp
@@ -0,0 +1,12 @@
+// stdafx.cpp : source file that includes just the standard includes
+// OpenMPSample.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#pragma once
+
+#include "stdafx.h"
+
+// TODO: reference additional headers your program requires here
+#include<stdio.h>
+#include <omp.h>
+#include<stdlib.h>
